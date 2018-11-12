9925faa93c31c6714934affd5e6f4059963e3685
wangtianyi1 <wangtianyi1@kingsoft.com>
2018-11-12 22:07:11 +0800

    将模板打包进二进制文件

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ go repo builder
 
 # 运行方式
 1. 依赖：Golang、Git，将当前用户的ssh key保存到git远端服务器
-2. 在本项目根目录下执行`go run main.go`
-3. 将生成的项目移动到$GOPATH/src
-4. 按照项目内的README文件安装依赖，运行应用
+2. 在本项目根目录下执行`go install`
+3. 在任意目录下执行`grb`
+4. 按照生成之后的项目内的README文件安装依赖，运行应用
 
 # 参考输入
-	GitHostAddress:   "http://wpsgit.kingsoft.net/",
-	GitServerVersion: "GitLab 6.3.0 LDAP",
 	RepoName:         "grbtest",
 	RepoNamespace:    "galaxy",
 	Username:         "wangtianyi1",
--- project/gomicrocli/creator.go
+++ project/gomicrocli/creator.go
@@ -1,21 +1,77 @@
 package gomicrocli
 
 import (
+	"archive/zip"
 	"fmt"
+	"github.com/rakyll/statik/fs"
 	"grb/common/util"
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	_ "statik"
+	"strings"
+	"unsafe"
 )
 
-func CreateProject(projectName string, path string) (err error) {
+//func CreateProject(projectName string, path string) (err error) {
+//	projectPath := projectName
+//	if path != "" {
+//		projectPath = path + "/" + projectName
+//	}
+//
+//	err = util.CopyDir("project/gomicrocli/microTemplate", projectPath)
+//	if err != nil {
+//		panic(err)
+//	}
+//	err = filepath.Walk(projectPath, util.Replace("microTemplate", projectName))
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Printf("Create project %s success", projectName)
+//	return err
+//}
+
+func CreateProject(projectName string, outputPath string) (err error) {
 	projectPath := projectName
-	if path != "" {
-		projectPath = path + "/" + projectName
+	if outputPath != "" {
+		projectPath = outputPath + "/" + projectName
 	}
 
-	err = util.CopyDir("project/gomicrocli/microTemplate", projectPath)
+	statikFS, err := fs.New()
 	if err != nil {
 		panic(err)
 	}
+	err = fs.Walk(statikFS, "/", func(path string, info os.FileInfo, err error) error {
+		ptrTof := unsafe.Pointer(&info)
+		ptrTof = unsafe.Pointer(uintptr(ptrTof) + uintptr(8)) // Or 4, if this is 32-bit
+		ptrToy := (**zip.FileHeader)(ptrTof)
+		_path := (*ptrToy).Name
+
+		if info.IsDir() && info.Name() == "/" {
+			return nil
+		}
+		if info.IsDir() && info.Name() != "/" {
+			return filepath.SkipDir
+		}
+
+		read, err := fs.ReadFile(statikFS, "/"+_path)
+		if err != nil {
+			panic(err)
+		}
+
+		if strings.Contains(_path, "/") {
+			os.MkdirAll(projectPath+"/"+_path[0:strings.LastIndex(_path, "/")], 0755)
+		}
+		err = ioutil.WriteFile(projectPath+"/"+_path, read, 0)
+		if err != nil {
+			panic(err)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	err = filepath.Walk(projectPath, util.Replace("microTemplate", projectName))
 	if err != nil {
 		panic(err)
